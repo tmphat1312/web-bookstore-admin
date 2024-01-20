cbcd959a03f2c8fa063ed2d27f2503ff9b7bbf23
Trương Minh Phát <tmphat1312@gmail.com>
2024-01-20 15:34:35 +0700

    refactor: Transform api response to a direct way (data not data.data)

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,6 +3,22 @@ import axios from "axios";
 const axiosClient = axios.create({
   withCredentials: true,
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  transformResponse: [
+    (raw) => {
+      const parsed = JSON.parse(raw);
+      const { data, error } = parsed;
+
+      if (error) {
+        return { error };
+      }
+
+      if (data) {
+        return data;
+      }
+
+      return parsed;
+    },
+  ],
 });
 
 export default axiosClient;
