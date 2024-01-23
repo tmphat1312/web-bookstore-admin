e5c9c8ea524ae90345b0072c89210047f18f8291
Trương Minh Phát <tmphat1312@gmail.com>
2024-01-23 20:10:53 +0700

    feat: Statistics page

--- src/features/dashboard/DashboardSalesChart.jsx
+++ src/features/dashboard/DashboardSalesChart.jsx
@@ -1,13 +1,24 @@
 import LoadingProgress from "../../ui/LoadingProgress";
-import ProfitChart from "./_ProfitChart";
-import RevenueChart from "./_RevenueChart";
+import ProfitChart from "./ProfitChart";
+import RevenueChart from "./RevenueChart";
 import { useSalesStatistics } from "./useSalesStatistics";
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
 
 export default function DashboardSalesChart() {
   const state = useSalesStatistics();
 
   return (
     <LoadingProgress {...state} resourceName="Thông kê doanh số và lợi nhuận">
+      <TableOperations end="true">
+        <Filter
+          filterField="type"
+          options={[
+            { label: "Trong 30 ngày", value: "month" },
+            { label: "Trong 12 tháng", value: "year" },
+          ]}
+        />
+      </TableOperations>
       <RevenueChart data={state.data} />
       <ProfitChart data={state.data} />
     </LoadingProgress>
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,7 +1,13 @@
+import StatisticsLayout from "../features/statistics/StatisticsLayout";
 import BackgroundHeading from "../ui/headings/BackgroundHeading";
 
 function Statistics() {
-  return <BackgroundHeading>Thông tin thống kê</BackgroundHeading>;
+  return (
+    <>
+      <BackgroundHeading>Thông tin thống kê</BackgroundHeading>
+      <StatisticsLayout />
+    </>
+  );
 }
 
 export default Statistics;
